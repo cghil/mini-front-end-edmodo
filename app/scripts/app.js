87f2fd593c4fed9c3c83aee9db96395a6b828bc0
cghil <cghilarducci@gmail.com>
2016-01-09 17:52:50 -0800

    added

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+<!doctype html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <title>Emodo Front End Project</title>
+    <meta name="description" content="Edmodo Front End Project">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="styles/main.css">
+  </head>
+  <body ng-app="myAuthApp">
+
+
+  <script src="vendor/scripts/angular.js"></script>
+  <script src="scripts/app.js"></script>
+
+  </body>
+</html>
